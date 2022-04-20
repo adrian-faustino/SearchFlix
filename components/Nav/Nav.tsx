ef12fb1286e3897d8d0c5780f72de0b2cde411c6
Adrian Faustino <adrian@getloop.ca>
2022-04-20 04:15:10 -0700

    Add nav bar routes

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,11 +1,24 @@
 import React, { FunctionComponent } from "react";
 
-import "./Nav.module.scss";
+import { APP_ROUTES } from "constants/index";
+import styles from "./Nav.module.scss";
 
 type INavProps = {};
 
 const Nav: FunctionComponent<INavProps> = () => {
-  return <div className="Nav">Nav</div>;
+  return (
+    <nav className={styles.Nav}>
+      <ol>
+        <li>
+          <a href={APP_ROUTES.welcome}>Home</a>
+        </li>
+
+        <li>
+          <a href={APP_ROUTES.search}>Search</a>
+        </li>
+      </ol>
+    </nav>
+  );
 };
 
 export default Nav;
