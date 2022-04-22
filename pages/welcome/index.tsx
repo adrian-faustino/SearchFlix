8c4d3e65cd131aecb979d6673c32c0b6453edd67
Adrian Faustino <adrian@getloop.ca>
2022-04-22 08:00:50 -0600

    Add home page

--- pages/welcome/index.tsx
+++ pages/welcome/index.tsx
@@ -7,34 +7,41 @@ import styles from "./welcome.module.scss";
 const Welcome: NextPage = () => {
   return (
     <div className={styles.welcome}>
-      <h3>Welcome!</h3>
-      <p>
-        This site can be used to search data about Movies, TV Series, and Games
-        by their title. Navigate to the{" "}
-        <Anchor href={APP_ROUTES.search}>Search</Anchor> page to begin your
-        search.
-      </p>
+      <section className={styles.welcome__about}>
+        <h1 className="color-attention margin-bottom-16">Welcome!</h1>
+        <p>
+          This site can be used to search data about Movies, TV Series, and
+          Games by their title.
+        </p>
+        <p>
+          Navigate to the{" "}
+          <Anchor className="bold" href={APP_ROUTES.search}>
+            Search
+          </Anchor>{" "}
+          page to begin your search.
+        </p>
 
-      <h4>
-        Here are links to some of the resources I used to create this website:
-      </h4>
-      <ul>
-        <li>
-          <Anchor isOpenNewTab href="https://github.com/adrian-faustino">
-            GitHub code repository
-          </Anchor>
-        </li>
-        <li>
-          <Anchor isOpenNewTab href="https://www.omdbapi.com/">
-            OMDb API
-          </Anchor>
-        </li>
-        <li>
-          <Anchor isOpenNewTab href="https://www.imdb.com/">
-            IMDd
-          </Anchor>
-        </li>
-      </ul>
+        <h4 className="margin-top-16">
+          Here are links to some of the resources I used to create this website:
+        </h4>
+        <ul>
+          <li>
+            <Anchor isOpenNewTab href="https://github.com/adrian-faustino">
+              GitHub code repository
+            </Anchor>
+          </li>
+          <li>
+            <Anchor isOpenNewTab href="https://www.omdbapi.com/">
+              OMDb API
+            </Anchor>
+          </li>
+          <li>
+            <Anchor isOpenNewTab href="https://www.imdb.com/">
+              IMDd
+            </Anchor>
+          </li>
+        </ul>
+      </section>
     </div>
   );
 };
